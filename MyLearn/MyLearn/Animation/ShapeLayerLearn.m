307101992be73c154df8333880c978ccd929d585
jianhua zhang <jianhuazhang@192.168.5.107>
2018-04-10 15:42:56 +0800

    注释
    注释

--- MyLearn/MyLearn/Animation/ShapeLayerLearn.m
+++ MyLearn/MyLearn/Animation/ShapeLayerLearn.m
@@ -13,43 +13,52 @@
 @implementation ShapeLayerLearn
 
 - (void)drawRect:(CGRect)rect{
+    
     CAShapeLayer *shapeLayer = [CAShapeLayer layer];
     shapeLayer.lineWidth = 3;
     
+    //贝塞尔路径
     UIBezierPath *path = [self blueToothPath];
     
     shapeLayer.path = path.CGPath;
     shapeLayer.fillColor = nil;
-    shapeLayer.strokeColor = [UIColor whiteColor].CGColor;
-    shapeLayer.strokeStart = 0;
-    shapeLayer.strokeEnd = 1;
     
+    //填充颜色
+    shapeLayer.strokeColor = [UIColor whiteColor].CGColor;
+    
+    //绘制起始点和终点
+    shapeLayer.strokeStart = 0.f;
+    shapeLayer.strokeEnd = 1.f;
+    
+    //第一个动画
     CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
     animation.fromValue = [NSNumber numberWithFloat:0.0f];
     animation.toValue = [NSNumber numberWithFloat:1.0f];
 //    [shapeLayer addAnimation:animation forKey:nil];
     
+    //第二个动画
     CABasicAnimation *animation1 = [CABasicAnimation animationWithKeyPath:@"strokeStart"];
     animation1.fromValue = [NSNumber numberWithFloat:1.0f];
     animation1.toValue = [NSNumber numberWithFloat:0.0f];
 //    [shapeLayer addAnimation:animation forKey:nil];
 
+    //组合动画
     CAAnimationGroup *groupAnimation = [CAAnimationGroup animation];
     groupAnimation.duration = 2.f;
-    //组合动画
     groupAnimation.animations = @[animation, animation1];
-    //结束时是否移除动画
-    groupAnimation.removedOnCompletion = NO;
+    //结束时是否手动移除动画 为NO需要手动释放代码在下面
+    groupAnimation.removedOnCompletion = YES;
     //重复次数
     groupAnimation.repeatCount = MAXFLOAT;
     groupAnimation.fillMode = kCAFillModeForwards;
-    [shapeLayer addAnimation:groupAnimation forKey:@""];
-    
+    [shapeLayer addAnimation:groupAnimation forKey:@"animation"];
+    //手动释放的代码
+//    [shapeLayer removeAnimationForKey:@"animation"];
     [self.layer addSublayer:shapeLayer];
     
 }
 - (UIBezierPath *)blueToothPath{
-    
+    //贝塞尔路径
     UIBezierPath *path = [UIBezierPath bezierPath];
     CGPoint p1 = CGPointMake(self.frame.size.width/2-blueToothWidth, self.frame.size.height/2-blueToothWidth);
     CGPoint p2 = CGPointMake(self.frame.size.width/2+blueToothWidth, self.frame.size.height/2+blueToothWidth);
@@ -66,5 +75,8 @@
     [path addLineToPoint:p6];
     
     return path;
+}
+- (void)removeFromSuperview{
+    //手动释放动画
 }
 @end

3ca855a36e6e1c52a08e811b353e508ea98dcb85
jianhua zhang <jianhuazhang@jianhuadeMacBook-Pro.local>
2018-12-05 23:21:27 +0800

    新建视频
    新建视频

--- MyLearn/MyLearn/Video/VideoViewController.m
+++ MyLearn/MyLearn/Video/VideoViewController.m
@@ -7,9 +7,16 @@
 //
 
 #import "VideoViewController.h"
-
+#import <AVFoundation/AVFoundation.h>
 @interface VideoViewController ()<UIImagePickerControllerDelegate,UINavigationControllerDelegate>
+//播放对象
+@property (nonatomic, strong) AVPlayer *player;
+//
+@property (nonatomic, strong) AVPlayerItem *currentPlayItem;
+//播放路径
+@property (nonatomic, copy) NSURL *videoUrl;
 
+@property (weak, nonatomic) IBOutlet UIButton *videoBtn;
 @end
 
 @implementation VideoViewController
@@ -19,9 +26,12 @@
     self.navigationController.title = @"视频剪辑";
 }
 - (IBAction)chooseAction:(id)sender {
-    [self chooseVideo];
+    if(!self.videoUrl)
+        [self chooseVideo];
+    else
+        [self loadVideo];
 }
-
+//选择视频
 - (void)chooseVideo{
     UIImagePickerController *pickerController = [[UIImagePickerController alloc]init];
     pickerController.sourceType = UIImagePickerControllerSourceTypePhotoLibrary;
@@ -34,10 +44,24 @@
     NSString *mediaType=[info objectForKey:UIImagePickerControllerMediaType];
     if ([mediaType isEqualToString:@"public.movie"]){
         //如果是视频
-        NSURL *url = info[UIImagePickerControllerMediaURL];//获得视频的URL
-        NSLog(@"url %@",url);
+        self.videoUrl = info[UIImagePickerControllerMediaURL];//获得视频的URL
+        [self loadVideo];
     }
     [self dismissViewControllerAnimated:YES completion:nil];
+}
+- (void)loadVideo{
+    AVPlayerItem *playerItem = [[AVPlayerItem alloc] initWithURL:self.videoUrl];
+    self.currentPlayItem = playerItem;
+    self.player = [AVPlayer playerWithPlayerItem:playerItem];
+    
+    AVPlayerLayer *avLayer = [AVPlayerLayer playerLayerWithPlayer:self.player];
+    avLayer.videoGravity = AVLayerVideoGravityResizeAspect;
+    avLayer.frame = self.videoBtn.bounds;
+    
+    self.videoBtn.backgroundColor = [UIColor blackColor];
+    [self.videoBtn.layer addSublayer:avLayer];
+    
+    [self.player play]; 
 }
 
 @end

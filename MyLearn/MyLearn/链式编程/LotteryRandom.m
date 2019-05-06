71e8435877cd7135196c07922a80cbeab98cb2ff
jianhzhang <pydai@grandstream.cn>
2019-05-06 09:45:44 +0800

    refresh
    refresh

--- MyLearn/MyLearn/链式编程/LotteryRandom.m
+++ MyLearn/MyLearn/链式编程/LotteryRandom.m
@@ -10,6 +10,7 @@
 
 @implementation LotteryRandom
 
+static int statistical;
 + (NSArray *)getResultNumber:(NSDictionary *)dict andCount:(int)count andKillNo:(NSArray *)killArr{
     if (dict == nil) {
         return @[];
@@ -21,24 +22,30 @@
     }
     //生成中奖号码
     NSMutableArray *arr = [NSMutableArray array];
+    statistical = 0;
     for (int i = 0; i < count; i++) {
         //每次获取概率总值
         NSString *key = [LotteryRandom getWinningNumber:data];
         [arr addObject:key];
+        statistical -= (int)[[data valueForKey:key] integerValue];
         [data removeObjectForKey:key];
     }
     return arr;
 }
 //生成中奖概率
 + (NSString *)getWinningNumber:(NSDictionary *)dict{
-    int statistical = 0;
     //中奖统计总和
-    for (NSString *countStr in [dict allValues]) {
-        statistical = statistical + (int)[countStr integerValue];
+    if (statistical == 0) {
+        for (NSString *countStr in [dict allValues]) {
+            statistical = statistical + (int)[countStr integerValue];
+        }
     }
-    NSLog(@"statistical = %d",statistical);
-    //获取中奖者
-    int random = (int)((long)([[NSDate date] timeIntervalSince1970] * 10000000) % arc4random() % statistical);
+    //获取中奖随机数
+    double timein = [[NSDate date] timeIntervalSince1970] * 10000000;
+    int arc = abs((int)arc4random());
+    
+    int random = (int)((long)timein % arc % statistical);
+    NSLog(@"statistical = %ld",(long)timein % arc);
     for (int i = 1; i <= dict.count; i++) {
         NSString *key = [NSString stringWithFormat:@"%@%d",i < 10 ? @"0":@"",i];
         int firstInterval = (int)[[dict valueForKey:key] integerValue];
@@ -47,6 +54,7 @@
             //获取得是index为i的key
             return key;
         }else{
+            //随机数减去未中区域
             random -= firstInterval;
         }
     }

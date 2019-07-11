a6aaedc5099b1567bce2a710cd94b7dbe009e848
jhzhang <jianhzhang@grandstream.cn>
2019-07-11 14:56:42 +0800

    add lottery
    add

--- MyLearn/MyLearn/彩票/LotteryViewController.m
+++ MyLearn/MyLearn/彩票/LotteryViewController.m
@@ -24,6 +24,11 @@
 
 @end
 
+typedef enum : NSUInteger {
+    LotterySuper = 1,
+    LotteryDouble = 10,
+} LotteryType;
+
 @implementation LotteryViewController
 
 - (IBAction)endEdit:(UITextField *)sender {
@@ -46,14 +51,15 @@
     self.redDoubleEntry.layer.borderColor = [UIColor groupTableViewBackgroundColor].CGColor;
     self.blueDoubleEntry.layer.borderColor = [UIColor groupTableViewBackgroundColor].CGColor;
     
-    [self initUserDefalut];
-    
-    [self initDoubleDefalut];
+//    [self initUserDefalut];
+//
+//    [self initDoubleDefalut];
     
     [ZNetServer postValueWithMethod:@"http://f.apiplus.net/dlt-20.json" andBody:nil successBlock:^(NSURLSessionDataTask * _Nonnull task, id _Nullable response) {
         self.arr = [response valueForKey:@"data"];
         self.openCode = [self.arr firstObject][@"opencode"];
-        [self updateData:self.arr];
+//        [self updateData:self.arr];
+        [self totalOpen:[response valueForKey:@"data"] withType:LotterySuper];
     } failBlock:^(NSURLSessionDataTask * _Nonnull task, NSError * _Nonnull error) {
         NSLog(@"2");
     }];
@@ -62,11 +68,119 @@
     [ZNetServer postValueWithMethod:@"http://f.apiplus.net/ssq-20.json" andBody:nil successBlock:^(NSURLSessionDataTask * _Nonnull task, id _Nullable response) {
         self.doubleArr = [response valueForKey:@"data"];
         self.doubleOpenCode = [self.doubleArr firstObject][@"opencode"];
-        [self updateDoubleData:self.doubleArr];
+//        [self updateDoubleData:self.doubleArr];
+        [self totalOpen:[response valueForKey:@"data"] withType:LotteryDouble];
     } failBlock:^(NSURLSessionDataTask * _Nonnull task, NSError * _Nonnull error) {
         NSLog(@"2");
     }];
 }
+- (void)totalOpen:(NSArray *)openArray withType:(LotteryType)type{
+    
+    [self initStartHitoryWithType:type];
+    NSMutableDictionary *redDict = [NSMutableDictionary dictionaryWithDictionary:[[NSUserDefaults standardUserDefaults] objectForKey:[NSString stringWithFormat:@"LotteryType%luFrontZone",(unsigned long)type]]];
+    NSMutableDictionary *blueDict = [NSMutableDictionary dictionaryWithDictionary:[[NSUserDefaults standardUserDefaults] objectForKey:[NSString stringWithFormat:@"LotteryType%luBackZone",(unsigned long)type]]];
+    
+    for (int i = 0; i < openArray.count; i ++) {
+        NSArray *opencode = [openArray[i][@"opencode"] componentsSeparatedByString:@"+"];
+        NSArray *redArr = [[opencode firstObject] componentsSeparatedByString:@","];
+        NSArray *blueArr = [[opencode lastObject] componentsSeparatedByString:@","];
+        for (NSString *str in redArr) {
+            [redDict setValue:[NSString stringWithFormat:@"%d",(int)[redDict[str] intValue] + 1] forKey:str];
+        }
+        for (NSString *str in blueArr) {
+            [blueDict setValue:[NSString stringWithFormat:@"%d",(int)[blueDict[str] intValue] + 1] forKey:str];
+        }
+    }
+    NSLog(@"type = %lu ---- \n  %@",(unsigned long)type,redDict);
+    NSLog(@"%@",blueDict);
+    [[NSUserDefaults standardUserDefaults] setObject:redDict forKey:[NSString stringWithFormat:@"LotteryType%luFrontZone",(unsigned long)type]];
+    [[NSUserDefaults standardUserDefaults] setObject:blueDict forKey:[NSString stringWithFormat:@"LotteryType%luBackZone",(unsigned long)type]];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+//初始化数据
+- (void)initStartHitoryWithType:(LotteryType)type{
+    int blue = 0;
+    int red = 0;
+    if (type == LotterySuper) {
+        red = 35;
+        blue = 12;
+    }else if (type == LotteryDouble){
+        red = 33;
+        blue = 16;
+    }
+    NSMutableDictionary *redDict = [NSMutableDictionary dictionary];
+    for (int i=1; i<=red; i++) {
+        NSString *key = [NSString stringWithFormat:@"%02d",i];
+        [redDict setValue:[NSNumber numberWithInt:1] forKey:key];
+    }
+    NSMutableDictionary *blueDict = [NSMutableDictionary dictionary];
+    for (int i=1; i<=blue; i++) {
+        NSString *key = [NSString stringWithFormat:@"%02d",i];
+        [blueDict setValue:[NSNumber numberWithInt:1] forKey:key];
+    }
+   
+    [[NSUserDefaults standardUserDefaults] setObject:redDict forKey:[NSString stringWithFormat:@"LotteryType%luFrontZone",(unsigned long)type]];
+    [[NSUserDefaults standardUserDefaults] setObject:blueDict forKey:[NSString stringWithFormat:@"LotteryType%luBackZone",(unsigned long)type]];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+}
+- (void)newShow1{
+    NSUserDefaults *data = [NSUserDefaults standardUserDefaults];
+    
+    LotteryType type = LotterySuper;
+    NSMutableDictionary *frontZone = [NSMutableDictionary dictionaryWithDictionary:[data valueForKey:[NSString stringWithFormat:@"LotteryType%luFrontZone",(unsigned long)type]]];
+    NSMutableDictionary *backZone = [NSMutableDictionary dictionaryWithDictionary:[data valueForKey:[NSString stringWithFormat:@"LotteryType%luBackZone",(unsigned long)type]]];
+    
+    type = LotteryDouble;
+    NSMutableDictionary *dfrontZone = [NSMutableDictionary dictionaryWithDictionary:[data valueForKey:[NSString stringWithFormat:@"LotteryType%luFrontZone",(unsigned long)type]]];
+    NSMutableDictionary *dbackZone = [NSMutableDictionary dictionaryWithDictionary:[data valueForKey:[NSString stringWithFormat:@"LotteryType%luBackZone",(unsigned long)type]]];
+    
+    NSArray *redKillArr;
+    NSArray *blueKillArr;
+    if (self.frontTF.text) {
+        redKillArr = [self.frontTF.text componentsSeparatedByString:@","];
+    }
+    if (self.backTF.text) {
+        blueKillArr = [self.backTF.text componentsSeparatedByString:@","];
+    }
+    
+    NSString *str;
+    if (redKillArr.count > 28 || blueKillArr.count > 10) {
+        
+        str = @"杀号超出限制";
+        
+    }else{
+        
+        NSArray *redArr = [LotteryRandom getResultNumber:frontZone andCount:5 + [self.redDoubleEntry.text intValue] andKillNo:redKillArr];
+        NSArray *blueArr = [LotteryRandom getResultNumber:backZone andCount:2 + [self.blueDoubleEntry.text intValue] andKillNo:blueKillArr];
+        
+        NSArray *result = [redArr sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+            return  [@([obj1 intValue]) compare:@([obj2 intValue])];
+        }];
+        NSArray *result1 = [blueArr sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+            return [@([obj1 intValue]) compare:@([obj2 intValue])];
+        }];
+        
+        NSArray *dredArr = [LotteryRandom getResultNumber:dfrontZone andCount:6 + [self.redDoubleEntry.text intValue] andKillNo:redKillArr];
+        NSArray *dblueArr = [LotteryRandom getResultNumber:dbackZone andCount:1 + [self.blueDoubleEntry.text intValue] andKillNo:blueKillArr];
+        
+        NSArray *dresult = [dredArr sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+            return  [@([obj1 intValue]) compare:@([obj2 intValue])];
+        }];
+        NSArray *dresult1 = [dblueArr sortedArrayUsingComparator:^NSComparisonResult(id  _Nonnull obj1, id  _Nonnull obj2) {
+            return [@([obj1 intValue]) compare:@([obj2 intValue])];
+        }];
+        str = [NSString stringWithFormat:@"随机\n大乐透:%@ + %@\n 双色球:%@ + %@",[result componentsJoinedByString:@","],[result1 componentsJoinedByString:@","],[dresult componentsJoinedByString:@","],[dresult1 componentsJoinedByString:@","]];
+    }
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:str message:[NSString stringWithFormat:@"上期结果\n大乐透:%@\n双色球:%@",_openCode,_doubleOpenCode] preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:^(UIAlertAction * _Nonnull action) {
+        
+    }]];
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+
+
 
 //更新近几期的统计数据
 - (void)updateDoubleData:(NSArray *)arr{
@@ -263,7 +377,7 @@
 }
 - (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
     [self.view endEditing:YES];
-    [self newShow];
+    [self newShow1];
 }
 - (void)newShow{
     NSUserDefaults *data = [NSUserDefaults standardUserDefaults];
